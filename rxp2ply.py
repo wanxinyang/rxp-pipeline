c251a6c34af4df532830e1eeacb4b906e73c2544
wanxinyang <w.yang.17@ucl.ac.uk>
2024-03-27 22:26:42 +0000

    add more outputs for the processing time and progress, add a flag --log to save the outputs into a log file

--- rxp2ply.py
+++ rxp2ply.py
@@ -17,8 +17,17 @@ import ply_io
 import pdal
 
 def tile_data(scan_pos, args):
+    # Get the scan name to use in log_file messages so that they are not too long:
+    scan_name = os.path.basename(scan_pos)
+    # Record the start time
+    start_time = datetime.now()
+    if args.verbose:
+        with args.Lock:
+            msg = f'[{datetime.now().strftime("%H:%M:%S")}] Worker started for scan: {scan_name}'
+            print(msg)
+            write_to_log(msg, args.log_file)
 
-    try:    
+    try:
         base, scan = os.path.split(scan_pos)
         try:
             if args.test:
@@ -26,46 +35,58 @@ def tile_data(scan_pos, args):
             else:
                 rxp = sorted(glob.glob(os.path.join(base, scan, 'scans' if 'SCNPOS' in scan else '', '??????_??????.rxp')))[-1]
         except:
-            if args.verbose: print(f"!!! Can't find {os.path.join(base, scan, '??????_??????.rxp')} !!!")
+            if args.verbose:
+                with args.Lock:
+                    msg = f"!!! Can't find {os.path.join(base, scan, '??????_??????.rxp')} !!!"
+                    print(msg)
+                    write_to_log(msg, args.log_file)
             return
+
         sp = int(scan.replace(args.prefix, '').replace('.SCNPOS', ''))
-    
+
         if args.verbose:
             with args.Lock:
-                print('rxp -> xyz:', rxp)
-    
+                rxp_name = os.path.basename(rxp)
+                msg = f'rxp -> xyz for scan pos {scan_name}: {rxp_name}'
+                print(msg)
+                write_to_log(msg, args.log_file)
+
         fn_matrix = glob.glob(os.path.join(args.matrix_dir, f'{scan.replace(".SCNPOS", "")}.*'))
-        if len(fn_matrix) == 0: 
-            if args.verbose: print('!!! Can not find rotation matrix', os.path.join(args.matrix_dir, scan.replace(".SCNPOS", "") + ".*"), '!!!')
+        if len(fn_matrix) == 0:
+            if args.verbose:
+                with args.Lock:
+                    msg = f"!!! Can not find rotation matrix: {os.path.join(args.matrix_dir, scan.replace('.SCNPOS', '') + '.*')} !!!"
+                    print(msg)
+                    write_to_log(msg, args.log_file)
             return
         matrix = np.dot(args.global_matrix, np.loadtxt(fn_matrix[0]))
         st_matrix = ' '.join(matrix.flatten().astype(str))
 
         cmds = []
- 
+
         # pdal commands as dictionaries
-        read_in = {"type":"readers.rxp",
+        read_in = {"type": "readers.rxp",
                    "filename": rxp,
                    "sync_to_pps": "false",
                    "reflectance_as_intensity": "false"}
         cmds.append(read_in)
-    
-        dev_filter = {"type":"filters.range", 
-                      "limits":"Deviation[0:{}]".format(args.deviation)}
-        cmds.append(dev_filter)    
 
-        refl_filter = {"type":"filters.range", 
-                      "limits":"Reflectance[{}:{}]".format(*args.reflectance)}
+        dev_filter = {"type": "filters.range",
+                      "limits": "Deviation[0:{}]".format(args.deviation)}
+        cmds.append(dev_filter)
+
+        refl_filter = {"type": "filters.range",
+                       "limits": "Reflectance[{}:{}]".format(*args.reflectance)}
         cmds.append(refl_filter)
-    
-        transform = {"type":"filters.transformation",
-                     "matrix":st_matrix}
+
+        transform = {"type": "filters.transformation",
+                     "matrix": st_matrix}
         cmds.append(transform)
-   
-        tile = {"type":"filters.splitter",
-                "length":f"{args.tile}",
-                "origin_x":"0",
-                "origin_y":"0"}
+
+        tile = {"type": "filters.splitter",
+                "length": f"{args.tile}",
+                "origin_x": "0",
+                "origin_y": "0"}
         cmds.append(tile)
 
         # link commmands and pass to pdal
@@ -73,52 +94,75 @@ def tile_data(scan_pos, args):
 
         pipeline = pdal.Pipeline(JSON)
         pipeline.execute()
-    
+
         # iterate over tiled arrays
         for arr in pipeline.arrays:
-    
+
             arr = pd.DataFrame(arr)
-            arr = arr.rename(columns={'X':'x', 'Y':'y', 'Z':'z'})
-            #arr.columns = ['x', 'y', 'z', 'InternalTime', 'ReturnNumber', 'NumberOfReturns',
+            arr = arr.rename(columns={'X': 'x', 'Y': 'y', 'Z': 'z'})
+            # arr.columns = ['x', 'y', 'z', 'InternalTime', 'ReturnNumber', 'NumberOfReturns',
             #               'amp', 'refl', 'EchoRange', 'dev', 'BackgroundRadiation', 
             #               'IsPpsLocked', 'EdgeOfFlightLine']
             arr.loc[:, 'sp'] = sp
-            arr = arr[['x', 'y', 'z', 'Reflectance', 'Deviation', 'ReturnNumber', 'NumberOfReturns', 'sp']] # save only relevant fields
- 
+            arr = arr[['x', 'y', 'z', 'Reflectance', 'Deviation', 'ReturnNumber', 'NumberOfReturns', 'sp']]  # save only relevant fields
+
             # remove points outside bbox
-            arr = arr.loc[(arr.x.between(args.bbox[0], args.bbox[2])) & 
+            arr = arr.loc[(arr.x.between(args.bbox[0], args.bbox[2])) &
                           (arr.y.between(args.bbox[1], args.bbox[3]))]
             if len(arr) == 0: continue
-    
+
             # identify tile number
             X, Y = (arr[['x', 'y']].min() // args.tile * args.tile).astype(int)
-            tile = args.tiles.loc[(args.tiles.x == X) & (args.tiles.y == Y)] 
-            if len(tile) == 0: continue   
+            tile = args.tiles.loc[(args.tiles.x == X) & (args.tiles.y == Y)]
+            if len(tile) == 0: continue
             tile_n = str(tile.tile.item()).zfill(args.n)
- 
+
             # save to xyz file
             with args.Lock:
                 if args.store_tmp_with_sp:
-                    with open(os.path.join(args.odir, f'{args.plot_code}{tile_n}.{sp}.xyz'), 'ab') as fh: 
-                        fh.write(arr.to_records(index=False).tobytes()) 
+                    with open(os.path.join(args.odir, f'{args.plot_code}{tile_n}.{sp}.xyz'), 'ab') as fh:
+                        fh.write(arr.to_records(index=False).tobytes())
                 else:
-                    with open(os.path.join(args.odir, f'{args.plot_code}{tile_n}.xyz'), 'ab') as fh: 
-                        fh.write(arr.to_records(index=False).tobytes()) 
+                    with open(os.path.join(args.odir, f'{args.plot_code}{tile_n}.xyz'), 'ab') as fh:
+                        fh.write(arr.to_records(index=False).tobytes())
 
-    except:
-        print('!!!!', scan_pos, '!!!!') 
-    
+        # Record the end time
+        end_time = datetime.now()
+        # Calculate the execution time
+
+        if args.verbose:
+            with args.Lock:
+                msg1 = f'[{datetime.now().strftime("%H:%M:%S")}] Worker finished for scan: {scan_name}'
+                msg2 = f'Runtime: {calculate_execution_time(start_time, end_time)}'
+                print(msg1)
+                print(msg2)
+                write_to_log(msg1, args.log_file)
+                write_to_log(msg2, args.log_file)
+
+    except Exception as e:
+        with args.Lock:
+            msg1 = f'!!!! {scan_pos} !!!!'
+            msg2 = f'{e}'
+            print(msg1)
+            print(msg2)
+            write_to_log(msg1, args.log_file)
+            write_to_log(msg2, args.log_file)
+
+
 def xyz2ply_w_sp(xyz, args):
+    start_time = datetime.now()
 
     xyz = str(xyz).zfill(args.n)
 
     if args.verbose:
         with args.Lock:
-            print(f'xyz -> ply: {xyz}')
+            msg = f'Worker process started for xyz -> ply: {xyz}'
+            print(msg)
+            write_to_log(msg, args.log_file)
 
     tmp = pd.DataFrame()
 
-    for fn in glob.glob(f'{xyz}.*.xyz'):    
+    for fn in glob.glob(f'{xyz}.*.xyz'):
         open_file = open(fn, encoding='ISO-8859-1')
         tmp = pd.concat([tmp, pd.DataFrame(np.fromfile(open_file, dtype='float64,float64,float64,float32,float32,uint8,uint8,int64'))])
         os.unlink(fn)
@@ -127,23 +171,67 @@ def xyz2ply_w_sp(xyz, args):
         tmp.columns = ['x', 'y', 'z', 'refl', 'dev', 'ReturnNumber', 'NumberOfReturns', 'sp']
         ply_io.write_ply(f'{xyz}.ply', tmp)
 
+    end_time = datetime.now()
+    if args.verbose:
+        with args.Lock:
+            msg1 = f'Worker process ended for xyz -> ply: {xyz}'
+            msg2 = f'Runtime: {calculate_execution_time(start_time, end_time)}'
+            print(msg1)
+            print(msg2)
+            write_to_log(msg, args.log_file)
+            write_to_log(msg, args.log_file)
+
+
 def xyz2ply(xyz_path, args):
+    start_time = datetime.now()
 
     if args.verbose:
         with args.Lock:
-            print('xyz -> ply:', xyz_path)
-    
+            msg = f'Worker process started for xyz -> ply: {xyz_path}'
+            print(msg)
+            write_to_log(msg, args.log_file)
+
     open_file = open(xyz_path, encoding='ISO-8859-1')
     tmp = pd.DataFrame(np.fromfile(open_file, dtype='float64,float64,float64,float32,float32,uint8,uint8,int64'))
     tmp.columns = ['x', 'y', 'z', 'refl', 'dev', 'ReturnNumber', 'NumberOfReturns', 'sp']
     ply_io.write_ply(xyz_path.replace('.xyz', '.ply'), tmp)
     os.unlink(xyz_path)
-    
+
+    end_time = datetime.now()
+    if args.verbose:
+        with args.Lock:
+            msg1 = f'Worker process ended for xyz -> ply: {xyz_path}'
+            msg2 = f'Runtime: {calculate_execution_time(start_time, end_time)}'
+            print(msg1)
+            print(msg2)
+            write_to_log(msg, args.log_file)
+            write_to_log(msg, args.log_file)
+
+
+def calculate_execution_time(start_time, end_time):
+    # Calculate the execution time
+    execution_time = end_time - start_time
+
+    # Convert the execution time to HH:MM:SS format
+    total_seconds = int(execution_time.total_seconds())
+    hours, remainder = divmod(total_seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    execution_time_str = f"{hours:02}:{minutes:02}:{seconds:02}"
+
+    return execution_time_str
+
+
+def write_to_log(message, log_file):
+    if log_file:
+        with open(log_file, 'a') as f:
+            f.write(message + '\n')
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--project', '-p', required=True, type=str, help='path to point cloud')
-    parser.add_argument('--matrix-dir', '-m',  type=str, default='', help='path to rotation matrices')
+    parser.add_argument('--matrix-dir', '-m', type=str, default='', help='path to rotation matrices')
     parser.add_argument('--plot-code', type=str, default='', help='plot suffix')
     parser.add_argument('--odir', type=str, default='.', help='output directory')
     parser.add_argument('--deviation', type=float, default=15, help='deviation filter')
@@ -162,23 +250,25 @@ if __name__ == '__main__':
     parser.add_argument('--pos', default=[], nargs='*', help='process using specific scan positions')
     parser.add_argument('--test', action='store_true', help='test using the .mon.rxp')
     parser.add_argument('--store-tmp-with-sp', action='store_true', help='spits out individual tmp files for tiles _and_ scan position')
+    parser.add_argument('--log-file', type=str, default='', help='log file')
     parser.add_argument('--verbose', action='store_true', help='print something')
 
     args = parser.parse_args()
-    args.project = os.path.abspath(args.project) 
+    args.project = os.path.abspath(args.project)
 
     # global rotation matrix
     if args.global_matrix:
         args.global_matrix = np.loadtxt(args.global_matrix)
-    else: args.global_matrix = np.identity(4)
-    
+    else:
+        args.global_matrix = np.identity(4)
+
     # find scans
     args.ScanPos = sorted(glob.glob(os.path.join(args.project, f'{args.prefix}*')))
     if len(args.ScanPos) == 0: raise Exception('no scan positions found')
     if args.plot_code != '': args.plot_code += '_'
 
     # find and read rotation matrix
-    if not os.path.isdir(args.matrix_dir): 
+    if not os.path.isdir(args.matrix_dir):
         raise Exception(f'no such directory: {args.matrix_dir}')
     M = glob.glob(os.path.join(args.matrix_dir, f'{args.prefix}*.*'))
     if len(M) == 0:
@@ -191,16 +281,16 @@ if __name__ == '__main__':
     if args.bounding_geometry and len(args.bbox) > 0:
         raise Exception('a bounding geometry and bounding box have been specified')
     if args.bbox:
-        geometry = Polygon(((args.bbox[0], args.bbox[1]), (args.bbox[0], args.bbox[3]), 
-                            (args.bbox[2], args.bbox[3]), (args.bbox[2], args.bbox[1]), 
+        geometry = Polygon(((args.bbox[0], args.bbox[1]), (args.bbox[0], args.bbox[3]),
+                            (args.bbox[2], args.bbox[3]), (args.bbox[2], args.bbox[1]),
                             (args.bbox[0], args.bbox[1])))
         extent = gp.GeoDataFrame([0], geometry=[geometry], columns=['id'])
     elif args.bounding_geometry:
-        extent = gp.read_file(args.bounding_geometry)#.buffer(args.buffer, join_style='mitre')
+        extent = gp.read_file(args.bounding_geometry)  # .buffer(args.buffer, join_style='mitre')
     elif args.rotate_bbox:
-        extent = gp.GeoDataFrame(data=np.arange(len(matrix_arr)), 
+        extent = gp.GeoDataFrame(data=np.arange(len(matrix_arr)),
                                  geometry=[Point(r[0], r[1]) for r in matrix_arr])
-        extent = gp.GeoDataFrame([0], columns=['id'], 
+        extent = gp.GeoDataFrame([0], columns=['id'],
                                  geometry=[extent.unary_union.minimum_rotated_rectangle.buffer(args.tile + args.buffer, join_style='mitre')])
     elif args.convex_hull:
         extent = gp.GeoDataFrame(data=np.arange(len(matrix_arr)),
@@ -208,33 +298,40 @@ if __name__ == '__main__':
         extent = gp.GeoDataFrame([0], columns=['id'],
                                  geometry=[extent.unary_union.convex_hull.buffer(args.tile + args.buffer)])
     else:
-        extent = gp.GeoDataFrame(data=np.arange(len(matrix_arr)), 
+        extent = gp.GeoDataFrame(data=np.arange(len(matrix_arr)),
                                  geometry=[Point(r[0], r[1]) for r in matrix_arr])
-        extent = gp.GeoDataFrame([0], columns=['id'], 
+        extent = gp.GeoDataFrame([0], columns=['id'],
                                  geometry=[extent.unary_union.envelope.buffer(args.tile + args.buffer, join_style='mitre')])
     args.bbox = (extent.exterior.bounds.values[0] // args.tile) * args.tile
     if args.verbose: print('bounding box:', args.bbox)
-    if args.save_bounding_geometry: 
+    if args.save_bounding_geometry:
         extent.to_file(args.save_bounding_geometry)
-        if args.verbose: print(f'bounding geometry saved to {args.save_bounding_geometry}') 
-    
+        if args.verbose:
+            msg = f'bounding geometry saved to {args.save_bounding_geometry}'
+            print(msg)
+            write_to_log(msg, args.log_file)
+
     # create tile db
     X, Y = np.meshgrid(np.arange(args.bbox[0], args.bbox[2], args.tile),
                        np.arange(args.bbox[1], args.bbox[3], args.tile))
     XY = np.vstack([X.flatten(), Y.flatten()]).T.astype(int)
     args.tiles = gp.GeoDataFrame(data=XY, columns=['x', 'y'], geometry=[Point(r[0], r[1]) for r in XY])
     args.tiles = gp.sjoin(args.tiles, extent, how='inner')
-    
+
     args.tiles.loc[:, 'tile'] = range(len(args.tiles))
     args.tiles = args.tiles[['x', 'y', 'tile', 'geometry']]
-    args.n = 4 #len(str(len(args.tiles)))
+    args.n = len(str(len(args.tiles)))
 
     if len(args.pos) > 0:
         args.pos = [os.path.abspath(p[:-1]) if p.endswith(os.pathsep) else os.path.abspath(p) for p in args.pos]
-        if args.verbose: print('processing only:', args.pos)
-        args.ScanPos =  list(args.pos) if len(args.pos) == 1 else args.pos
-        #args.ScanPos = [os.path.join(args.project, p) for p in args.pos]
+        if args.verbose:
+                msg = f'processing only: {args.pos}'
+                print(msg)
+                write_to_log(msg, args.log_file)
 
+        args.ScanPos = list(args.pos) if len(args.pos) == 1 else args.pos
+        # args.ScanPos = [os.path.join(args.project, p) for p in args.pos]
+        
     # write tile index
     args.tiles[['tile', 'x', 'y']].to_csv(os.path.join(args.odir, 'tile_index.dat'), 
                                           sep=' ', index=False, header=False)
@@ -244,8 +341,8 @@ if __name__ == '__main__':
     Pool = multiprocessing.Pool(args.num_prcs)
     m = multiprocessing.Manager()
     args.Lock = m.Lock()
-    #[tile_data(sp, args) for sp in np.sort(args.ScanPos)]
-    Pool.starmap(tile_data, [(sp, args) for sp in np.sort(args.ScanPos)])
+    # [tile_data(sp, args) for sp in np.sort(args.ScanPos)] # run in series
+    Pool.starmap(tile_data, [(sp, args) for sp in np.sort(args.ScanPos)]) # run in parallel
 
     # write to ply - reusing Pool
     if args.store_tmp_with_sp:
@@ -253,9 +350,11 @@ if __name__ == '__main__':
     else:
         xyz = glob.glob(os.path.join(args.odir, '*.xyz'))
         Pool.starmap_async(xyz2ply, [(xyz, args) for xyz in np.sort(xyz)])
-    #[xyz2ply(xyz, args) for xyz in np.sort(args.tiles.tile)]
+    # [xyz2ply(xyz, args) for xyz in np.sort(args.tiles.tile)]
     Pool.close()
     Pool.join()
 
 
-    print(f'runtime: {(datetime.now() - start).seconds}')
+    end = datetime.now()
+    msg = f'Total rxp2ply runtime: {calculate_execution_time(start, end)}'
+    print(msg)
